f541da5cfc611948468240ee85d806129ca61a40
petja <hello@petja.me>
2022-07-09 22:02:40 +0300

    Fööni video

--- next.config.js
+++ next.config.js
@@ -3,16 +3,23 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["www.gravatar.com"],
+    domains: [
+      'www.gravatar.com',
+      'media1.tunn3l.com',
+      'media2.tunn3l.com',
+      'media3.tunn3l.com',
+      'media4.tunn3l.com',
+      'media5.tunn3l.com',
+    ],
   },
   async rewrites() {
     return [
       {
-        source: "/ig/:path*",
-        destination: "/:path*",
+        source: '/ig/:path*',
+        destination: '/:path*',
       },
-    ];
+    ]
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- pages/tunnel.tsx
+++ pages/tunnel.tsx
@@ -1,68 +1,71 @@
-import { Topbar } from "../components/Topbar";
-import { Container } from "../components/Container";
-import Head from "next/head";
-import Image from "next/image";
+import { useEffect, useState } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+
+import useSWR from 'swr'
+
+import { Topbar } from '../components/Topbar'
+import { Container } from '../components/Container'
 
 const skills = [
   {
-    name: "Belly flying",
+    name: 'Belly flying',
   },
   {
-    name: "Back flying",
+    name: 'Back flying',
   },
   {
-    name: "Belly carving",
+    name: 'Belly carving',
   },
   {
-    name: "Barrel rolls",
+    name: 'Barrel rolls',
   },
   {
-    name: "Knee flying",
+    name: 'Knee flying',
   },
   {
-    name: "Sit flying",
+    name: 'Sit flying',
   },
   {
-    name: "Belly-back-belly transitions",
+    name: 'Belly-back-belly transitions',
   },
   {
-    name: "The very basics of head down",
+    name: 'The very basics of head down',
   },
-];
+]
 
 const skillsUpcoming = [
   {
-    name: "Back carving",
+    name: 'Back carving',
   },
   {
-    name: "Vertical big-way",
+    name: 'Vertical big-way',
   },
   {
-    name: "Dynamic flying",
+    name: 'Dynamic flying',
   },
-];
+]
 
-/*
-        <p>
-          <strong>Tunnel flying</strong> (a.k.a. <em>indoor skydiving</em>) is a
-          hobby in vertical wind tunnel where you &hellip;
-          <ul className="list-disc ml-8">
-            <li>Exercise to fly with your body in certain positions</li>
-            <li>
-              Exercise to transition from one position to another, smoothly
-            </li>
-            <li>
-              Exercise to do the aforementioned with other flyers together
-            </li>
-            <li>Have a lot of fun!</li>
-          </ul>
-        </p>
-*/
-
-const hours = 15;
-const costPerHour = 590;
+const hours = 15
+const costPerHour = 590
 
 export default function Tunnel() {
+  const { data, error } = useSWR('/api/fooni_video', fetch)
+  const [latestVideo, setLatestVideo] = useState<{
+    title: string
+    date: string
+    posterUrl: string
+    downloadUrl: string
+  } | null>()
+
+  useEffect(() => {
+    if (data && !data.bodyUsed) {
+      data.json().then((data) => {
+        setLatestVideo(data.latestVideo)
+      })
+    }
+  }, [data])
+
   return (
     <>
       <Head>
@@ -70,22 +73,60 @@ export default function Tunnel() {
       </Head>
       <Topbar
         heading="Tunnel flying"
-        subtitle="Last updated on July 7th, 2022"
+        subtitle="Last updated on July 10, 2022"
       />
       <Container className="space-y-8 pb-20 animate-fadeInUp">
+        {latestVideo ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="flex flex-col gap-4">
+              <h1 className="font-bold text-lg text-white">Latest video</h1>
+              <span>
+                Video recorded at
+                <br />
+                {new Date(latestVideo.date).toLocaleString(['en'], {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                  hour: 'numeric',
+                  minute: 'numeric',
+                  hour12: false,
+                })}
+              </span>
+              <a href={latestVideo.downloadUrl} target="_newtab">
+                View video &rarr;
+              </a>
+            </div>
+            <div>
+              <a href={latestVideo.downloadUrl} target="_newtab">
+                <Image
+                  src={latestVideo.posterUrl}
+                  alt="Latest video"
+                  width="300"
+                  height="168"
+                  className="rounded-xl"
+                />
+              </a>
+            </div>
+          </div>
+        ) : error ? (
+          <p>Could not retrieve latest video 😞</p>
+        ) : (
+          <p>Loading video ...</p>
+        )}
+
         <h1 className="font-bold text-lg text-white">Stats</h1>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 text-slate-300">
           <div>
-            Currently I have had around{" "}
+            Currently I have had around{' '}
             <span className="text-xl font-bold block text-white">
               {hours} hours
-            </span>{" "}
+            </span>{' '}
             of time in a tunnel
           </div>
 
           <div>
-            This have cost me around{" "}
+            This have cost me around{' '}
             <span className="text-xl font-bold block text-white">
               {((hours * costPerHour) / 1_000).toFixed(1)} k€
             </span>
@@ -118,10 +159,10 @@ export default function Tunnel() {
             alt="Instagram logo"
           />
           <div>
-            Follow my learning path on my{" "}
+            Follow my learning path on my{' '}
             <a href="https://instagram.com/petjato" target="_newtab">
               Instagram
-            </a>{" "}
+            </a>{' '}
             stories
           </div>
         </div>
@@ -148,5 +189,5 @@ export default function Tunnel() {
         </div>
       </Container>
     </>
-  );
+  )
 }
